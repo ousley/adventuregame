e01d80f0c766d69d2a59a0a66344582ec6a8a93e
ousley <ousley344@gmail.com>
2017-12-05 00:23:49 -0700

    Cleaned-up version for submission

--- prog.pl
+++ prog.pl
@@ -1,6 +1,87 @@
-:- ['sentence.pl'].
+%%% sentence parsing
 
-% TODO: generalized name matching that almost everything will need
+getsentence(Words, In, Out) :-
+   get0(C),
+%split on periods
+   ((C = 46, name(W, In), append(Words,[W],Out), !); 
+%break on spaces
+    (C = 32, name(W, In), append(Words,[W],Words1), getsentence(Words1, [], Out), !);
+%break on newlines
+    (C = 10, name(W, In), append(Words,[W],Words2), getsentence(Words2, [], Out), !);
+    append(In,[C],In1), getsentence(Words, In1,Out)).
+
+getsentence(Words) :-
+   getsentence([],[],Words).
+
+% Read sentence and map chars to atoms.
+readsentence(S1) :-
+   getsentence(R),
+   mapwords(R, S),
+   strip(S, S1).
+
+% Strip blanks
+strip_initial([],[]).
+strip_initial([blank|S],R) :-
+   !, strip_initial(S,R).
+strip_initial(S,S).
+
+strip_final(S,R) :-
+   reverse(S,S1),
+   strip_initial(S1,S2),
+   reverse(S2,R).
+
+strip_inside([],[]).
+strip_inside([blank,blank|S],R) :-
+   !, strip_inside([blank|S],R).
+strip_inside([blank|S],[blank|R]) :-
+   !, strip_inside(S,R).
+strip_inside([C|S],[C|R]) :-
+   strip_inside(S,R).
+
+strip(S,R):-
+   strip_initial(S,S1),
+   strip_inside(S1,S2),
+   strip_final(S2, R).
+
+% Split words into letters.
+mapwords([W|WL],S) :-
+   name(W, N),
+   mapchars(N, S1),
+   mapwords(WL, S2),
+   append(S1,[blank|S2], S).
+mapwords([],[]).
+
+mapchars([C|N], [C1|S]) :-
+   mapchar([C], C1),
+   mapchars(N, S).
+mapchars([],[]).
+
+mapchar("A",a). mapchar("B",b). mapchar("C",c). mapchar("D",d). mapchar("E",e).
+mapchar("F",f). mapchar("G",g). mapchar("H",h). mapchar("I",i). mapchar("J",j).
+mapchar("K",k). mapchar("L",l). mapchar("M",m). mapchar("N",n). mapchar("O",o).
+mapchar("P",p). mapchar("Q",q). mapchar("R",r). mapchar("S",s). mapchar("T",t).
+mapchar("U",u). mapchar("V",v). mapchar("W",w). mapchar("X",x). mapchar("Y",y).
+mapchar("Z",z).
+
+mapchar("a",a). mapchar("b",b). mapchar("c",c). mapchar("d",d). mapchar("e",e).
+mapchar("f",f). mapchar("g",g). mapchar("h",h). mapchar("i",i). mapchar("j",j).
+mapchar("k",k). mapchar("l",l). mapchar("m",m). mapchar("n",n). mapchar("o",o).
+mapchar("p",p). mapchar("q",q). mapchar("r",r). mapchar("s",s). mapchar("t",t).
+mapchar("u",u). mapchar("v",v). mapchar("w",w). mapchar("x",x). mapchar("y",y).
+mapchar("z",z).
+
+mapchar("!", excl_mark).  mapchar("?", quest_mark).
+mapchar(".", full_stop).  mapchar(",", comma).
+mapchar("-", hyphen).     mapchar(";", semicolon).
+mapchar("$", dollar).     mapchar("'",apostrophe).
+
+mapchar("0", 0). mapchar("1", 1). mapchar("2", 2). mapchar("3", 3). 
+mapchar("4", 4). mapchar("5", 5). mapchar("6", 6). mapchar("7", 7). 
+mapchar("8", 8). mapchar("9", 9). 
+
+mapchar(X, X).
+
+
 
 %%% Definitions
 
@@ -97,9 +178,9 @@ printget(ShortName) :-
 	member(ShortName,ShortNames),
 	location(player,Rm),
 	location(Obj,Rm),
-	retract(progress(X)),
-	assert(progress(Y)),
-	Y is X + 20,
+	%retract(progress(X)),
+	%assert(progress(Y)),
+	%Y is X + 20,
 	!,
 	format('~s isn\'t something you can carry.~n', [ObjNm]).
 % "get" with unknown target
@@ -118,9 +199,9 @@ printdrop(ShortName) :-
 	!,
 	retract(location(Item,inventory)),
 	assert(location(Item,Rm)),
-	retract(progress(X)),
-	assert(progress(Y)),
-	Y is X + 20,
+	%retract(progress(X)),
+	%assert(progress(Y)),
+	%Y is X + 20,
 
 	% cut so we only drop one item at a time if there are duplicates
 	!,
@@ -175,8 +256,8 @@ printinv(_) :- printinv.
 % "status" - show turn count and score (santa part totals)
 printstatus :-
 	timer(global,T),
-	progress(P),
-	format('You have taken ~d turns and collected ~d percent of santa parts.', [T]).
+	partcount(P),
+	format('You have taken ~d turns and collected ~d/5 of santa\'s parts.~n', [T,P]).
 printstatus(_) :- printstatus.
 
 % "help" with no target - general help/commands
@@ -209,7 +290,6 @@ printwin :-
 printwin(_) :- printwin.
 
 % Quit the game.
-% TODO: does not work
 quit :- running(true),
 	writeln("You quit."),
 	retract(running(true)).
@@ -290,6 +370,7 @@ parse([LVb|LTgt]) :-
 parse([LTgt]) :-
 	printgo(LTgt).
 
+% Generate a call to one of the defined verbs, depending on whether a target was given.
 buildcall(Vb,Tgt) :-
 	Tgt = [],
 	!,
@@ -305,6 +386,17 @@ etick :-
 	Tn is T+1,
 	retract(timer(N,T)),
 	assert(timer(N,Tn)).
+
+partcount(Ct) :-
+	% create a list L of all santa parts the player has
+	findall(P, havepart(P),L),
+	% get its length
+	length(L,Ct).
+
+% Check whether P is a santa part and in the player's inventory.
+havepart(P) :-
+	santapart(P),
+	location(P,inventory).
 
 % The player runs this to start the game, printing introductory stuff and starting the main loop.
 start :-
@@ -356,6 +448,12 @@ item(santaArms).
 item(santaBody).
 item(santaOutfit).
 
+santapart(santaHead).
+santapart(santaLegs).
+santapart(santaArms).
+santapart(santaBody).
+santapart(santaOutfit).
+
 talker(player).
 talker(jacketMan).
 location(streetlamp,streetCorner).
@@ -385,7 +483,7 @@ location(deer, centralPark).
 location(elves,timesSquare).
 
 
-% an example
+% old example rooms
 
 room(streetCorner, "a street corner").
 room(bealeSt, "an east/west street").
@@ -406,7 +504,7 @@ room(empireState, "the Empire State Building").
 room(unionSquare, "Union Square").
 room(littleItaly, "Little Italy").
 
-
+% old example exits
 
 exit(streetCorner,bealeSt,east).
 exit(streetCorner,parkAve,south).
